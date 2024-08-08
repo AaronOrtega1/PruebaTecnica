0ad09aa214910c4d4129dbe401e07bf4f79c6431
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-08-08 12:39:42 -0600

    commented modal appearence and some lines for ¿documentation?, started README and added some style in css

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # PruebaTecnica
 
-- icons:
+### Explanation
+
+This proyect is a user profile page with different sections about skills, experience and more, has a default dark theme and a color button to switch the theme to a light one, navigation buttons in the header to move to the different sections in the website, each time the page its reloaded a modal will appear gretting the person checking the website, at the end in the contact section there is a non-functional comment section, that has to keep the messages in the local storage of the website.
+
+---
+
+### How to run it
+
+To run this proyect is necessary to have npm install in your computer
+
+- Once you have npm installed run the following command in your terminal inside the project folder:
+  ```bash
+    npm install
+  ```
+  - This command will install the node dependece of lite-server, that will allow you to run the project in your computer
+- To run the server and view the website in the browser you will have to run the followig command:
+  ```bash
+    npm run dev
+  ```
+  - This will launch the server in your localhost, when it start in your terminal will appear a localhost port that you will have to put in your browser.
+    - example with port 3000: **localhost:3000**
+
+Another alternative is to run it using the Visual Studio Code extension `Live Server` by Ritwick Dey, this will host the port 5500 in your localhost -> **localhost:5500**
+
+---
+
+### External resources used
+
+- Icons:
   - https://fontawesome.com/
--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 function changeColor() {
+  // Get the button and body of html
   const toggleButton = document.getElementById("theme-toggle");
   const body = document.body;
 
   toggleButton.addEventListener("click", () => {
+    // Toggle class in body to light-theme on click
     body.classList.toggle("light-theme");
   });
 }
 
+//
 document.addEventListener("DOMContentLoaded", changeColor);
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the modal
-  var modal = document.getElementById("welcome-modal");
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get the modal
+//   var modal = document.getElementById("welcome-modal");
 
-  // Get the <span> element that closes the modal
-  var closeButton = document.querySelector(".close-button");
+//   // Get the <span> element that closes the modal
+//   var closeButton = document.querySelector(".close-button");
 
-  // Open the modal
-  modal.style.display = "block";
+//   // Open the modal
+//   modal.style.display = "block";
 
-  // When the user clicks on <span> (x), close the modal
-  closeButton.onclick = function () {
-    modal.style.display = "none";
-  };
+//   // When the user clicks on <span> (x), close the modal
+//   closeButton.onclick = function () {
+//     modal.style.display = "none";
+//   };
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-});
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function (event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   };
+// });
